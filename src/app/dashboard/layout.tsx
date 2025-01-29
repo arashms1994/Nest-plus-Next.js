2ff1969db25caecdeb90439723b941bef152b022
arashmogh <arash.ms1994@gmail.com>
2025-01-29 19:36:53 +0330

    Feat: add admin dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,11 @@
+import DashboardHeader from "@/components/dashboard-layout/dashboard-header";
+import DrawerHeader from "@/components/dashboard-layout/drawer-header";
+import MiniDrawer from "@/components/dashboard-layout/mini-drawer";
 import { auth } from "@/lib/session";
 import AuthProvider from "@/providers/AuthProvider";
+import DrawerProvider from "@/providers/DrawerProvider";
 import QueryProvider from "@/providers/QueryProvider";
+import { Box } from "@mui/material";
 
 async function DashboardLayout({
   children,
@@ -10,7 +15,18 @@ async function DashboardLayout({
   const { accessToken } = await auth();
   return (
     <AuthProvider accessToken={accessToken || ""}>
-      <QueryProvider>{children}</QueryProvider>
+      <QueryProvider>
+        <Box sx={{ display: "flex" }}>
+          <DrawerProvider>
+            <DashboardHeader />
+            <MiniDrawer />
+          </DrawerProvider>
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <DrawerHeader />
+            {children}
+          </Box>
+        </Box>
+      </QueryProvider>
     </AuthProvider>
   );
 }
