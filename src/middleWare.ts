57c97dbb1b9e797f0d76061b6b1b04796bce6ef4
arashmogh <arash.ms1994@gmail.com>
2025-02-06 16:01:50 +0330

    Fix: middleware for shop rout

--- src/middleWare.ts
+++ src/middleWare.ts
@@ -2,35 +2,40 @@ import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { refreshTokenAction } from "./actions/auth/refresh-token";
 
-const protectedRoutes = "/dashboard";
-const publicRoutes = "/";
+const protectedRoutes: string[] = ["/dashboard", "/shop"];
+const publicRoutes: string[] = ["/", "/auth/login"];
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
-  const isProtectedRoute = path.startsWith(protectedRoutes);
-  const isPublicRoute = path.startsWith(publicRoutes);
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    path.startsWith(route)
+  );
+  const isPublicRoute = publicRoutes.includes(path);
 
   const accessToken = (await cookies()).get("accessToken")?.value;
   const refreshToken = (await cookies()).get("refreshToken")?.value;
-  const isLogin = accessToken && refreshToken;
+  const isLogin = !!accessToken && !!refreshToken;
   const isLogout = !accessToken && !refreshToken;
-  const needToRefresh = !accessToken && refreshToken;
+  const needToRefresh = !accessToken && !!refreshToken;
+
   if (needToRefresh) {
-    await refreshTokenAction();
-    return NextResponse.redirect(new URL(req.nextUrl, req.nextUrl));
+    const refreshed = await refreshTokenAction();
+    if (refreshed !== undefined && refreshed) {
+      return NextResponse.redirect(new URL(path, req.nextUrl));
+    }
   }
+
   if (isProtectedRoute && isLogout) {
     return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
   }
 
-  if (isPublicRoute && !isProtectedRoute && isLogin) {
+  if (isPublicRoute && isLogin) {
     return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
   }
 
   return NextResponse.next();
 }
 
-// Routes Middleware should not run on
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
