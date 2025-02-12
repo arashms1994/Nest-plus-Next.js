2636a734379f46fb15ca8d6341b00563bb5530f6
arashmogh <arash.ms1994@gmail.com>
2025-02-13 01:18:56 +0330

    Fix: navbar padding

--- src/components/home-components/navbar/navbar.tsx
+++ src/components/home-components/navbar/navbar.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-10">
       <Sheet>
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="lg:hidden">
